26b9d85cc5b048bee25b448019facfa415d21a52
ttnghia@cmc.com.vn <ttnghia@cmc.com.vn>
2022-02-26 07:54:59 +0000

    edi

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,6 +1,6 @@
 select 
 T.orderid as order_id,
 -- amount is stored in cents, convert it to dollars
-T.amount / 100 as amount,
+T.amount / 100 as amount
 from {{ source('stripe', 'payment') }} T
 Where T.status = 'success'
