489501751ebc0ddacda3534c18d7528a05cf0c75
ttnghia@cmc.com.vn <ttnghia@cmc.com.vn>
2022-02-26 13:23:22 +0000

    test

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,6 +1,9 @@
-select 
-T.orderid as order_id,
--- amount is stored in cents, convert it to dollars
-T.amount / 100 as amount
-from {{ source('stripe', 'payment') }} T
-Where T.status = 'success'
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount', 4) }} as amount,
+    created as created_at
+from {{ source('stripe','payment') }}
